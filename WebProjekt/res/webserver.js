4d876d6d924a00b13527a788d8c05ec674dc65b5
GeeHopper <33354748+GeeHopper@users.noreply.github.com>
2020-05-16 19:33:40 +0200

    added server tools

--- WebProjekt/res/webserver.js
+++ WebProjekt/res/webserver.js
@@ -1,8 +1,25 @@
 var http = require("http");
 var url = require("url");
-var chalk = require("chalk")
+var chalk = require("chalk");
+var formidable = require("formidable");
+var fs = require("fs");
+
+//self written requirements
+var date = require("./node_modules/dateTools");
+
+//reading file from server
+/*fs.readFile("test.txt", function(error, data){
+    //console.log(data)//binary output
+    console.log(data.toString);
+})*/
 
 
+//writing file to server
+/*fs.writeFile("newFile.txt", "Inhalt der\nDatei", (error) => {
+    if(error)
+        throw error;
+    console.log("file was saved");
+});*/
 
 //passed console arguments
 /*var arguments = process.argv;
@@ -12,29 +29,53 @@ console.log(arguments[2]);*/
 /*console.log("default");
 console.log(chalk.red("rot"));*/
 
+//dateoutput
+//console.log(date.germanDate());
+
 http.createServer(function(request, response) {
-    response.writeHead(200, {"content-type": "text/html; charset=utf-8"});
-    
-    var body =
-        "<form action='fileupload' method='post' enctype='multipart/form-data'>"+
-        "<input type='file' name='file'><br>"+
-        "<input type='submit'>"+
-        "</form>";
-    var htmlResponse = getHTMLStruct("MediComm", body);
 
+    if (request.url == "/fileupload"){
+        var form = new formidable.IncomingForm();
+        form.parse(request, function(error, fields, files){
+            var oldpath = files.file.path;
+            var newpath = __dirname+"/files/"+files.file.name;
 
-    var urlRequest = url.parse(request.url, true);
-    var name = "";
-
-
-
-    if(urlRequest.query.name != undefined) {
-        name = urlRequest.query.name;
+            fs.rename(oldpath, newpath, function(error){
+                if(error)
+                    throw error;
+                response.write("File uploaded");
+                response.end();
+            });
+        });
     }
 
-    response.write("Guten Tagg " + name);
+    else
+    {
 
-    response.end(htmlResponse);
+
+        response.writeHead(200, {"content-type": "text/html; charset=utf-8"});
+        
+        var body =
+            "<form action='fileupload' method='post' enctype='multipart/form-data'>"+
+            "<input type='file' name='file'><br>"+
+            "<input type='submit'>"+
+            "</form>";
+        var htmlResponse = getHTMLStruct("MediComm", body);
+
+
+        var urlRequest = url.parse(request.url, true);
+        var name = "";
+
+
+
+        if(urlRequest.query.name != undefined) {
+            name = urlRequest.query.name;
+        }
+
+        response.write("Guten Tag " + name);
+
+        response.end(htmlResponse);
+    }
 }).listen(8080, "127.0.0.1");
 console.log("Webserver wird ausgeführt.");
 
