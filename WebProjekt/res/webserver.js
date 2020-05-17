803ce0be53a972967c31a034457e1de1684e767b
GeeHopper <33354748+GeeHopper@users.noreply.github.com>
2020-05-17 11:43:31 +0200

    start using expressjs

--- WebProjekt/res/webserver.js
+++ WebProjekt/res/webserver.js
@@ -6,6 +6,7 @@ var fs = require("fs");
 var util = require("util");
 var events = require("events")
 var mysql = require("mysql");
+var express = require("express")
 
 //self written requirements
 var date = require("./node_modules/dateTools");
@@ -53,11 +54,85 @@ eventEmitter.on("myEvent", eventHandler);
 
 
 
+var app = express();
 
-http.createServer(function(request, response) {
+app.get("/", function(request, response){
+    
 
-    if (request.url == "/fileupload"){
-        var form = new formidable.IncomingForm();
+    //response.writeHead(200, {"content-type": "text/html; charset=utf-8"});
+    
+
+    //MySQL Database tools
+    /*
+    var con = mysql.createConnection({
+        host: "localhost",
+        database: "medicomm",
+        user: "root",
+        password: ""
+    });
+
+    var message = "msgtemplate";
+
+    eventEmitter.on("db-ready", sendResponse);
+    con.connect(function(error){
+        if(error){
+            message = "Connection error: " + error.message;
+            eventEmitter.emit("db-ready");
+            throw error;
+        }
+        else{
+            con.query("SELECT * FROM users",
+            function(error, result, fields){
+                if(error){
+                    
+                    message = "Query error: " + error.message;
+                    eventEmitter.emit("db-ready");
+                }
+                else{
+                    message = result[0].mail;
+                    eventEmitter.emit("db-ready");
+                }
+            });
+            
+        }
+    });
+
+    function sendResponse(){
+        var htmlResponse = getHTMLStruct("MediComm",
+        "<h1 style='color:red;'>" + message + "</h1>");
+    
+        response.end(htmlResponse);
+    }*/
+
+    //File upload
+    
+    var body =
+        "<form action='fileupload' method='post' enctype='multipart/form-data'>"+
+        "<input type='file' name='file'><br>"+
+        "<input type='submit'>"+
+        "</form>";
+    var htmlResponse = getHTMLStruct("MediComm", body);
+
+
+    var urlRequest = url.parse(request.url, true);
+    var name = "";
+
+
+
+    if(urlRequest.query.name != undefined) {
+        name = urlRequest.query.name;
+    }
+
+    response.send(htmlResponse);
+
+    //response.write("Guten Tag " + name);
+
+    //response.end(htmlResponse);
+
+});
+
+app.post("/fileupload", function(request, response){
+    var form = new formidable.IncomingForm();
         form.parse(request, function(error, fields, files){
             var oldpath = files.file.path;
             var newpath = __dirname+"/files/"+files.file.name;
@@ -69,79 +144,14 @@ http.createServer(function(request, response) {
                 response.end();
             });
         });
-    }
+});
 
-    else
-    {
+var server = app.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-
-        response.writeHead(200, {"content-type": "text/html; charset=utf-8"});
-        
-        var con = mysql.createConnection({
-            host: "localhost",
-            database: "medicomm",
-            user: "root",
-            password: ""
-        });
-
-        var message = "msgtemplate";
-
-        eventEmitter.on("db-ready", sendResponse);
-        con.connect(function(error){
-            if(error){
-                message = "Connection error: " + error.message;
-                eventEmitter.emit("db-ready");
-                throw error;
-            }
-            else{
-                con.query("SELECT * FROM users",
-                function(error, result, fields){
-                    if(error){
-                        
-                        message = "Query error: " + error.message;
-                        eventEmitter.emit("db-ready");
-                    }
-                    else{
-                        console.log(result);
-                        console.log(result[0].mail);
-                        eventEmitter.emit("db-ready");
-                        console.log("emitted :)");
-                    }
-                });
-                
-            }
-        });
-
-        function sendResponse(){
-            var htmlResponse = getHTMLStruct("MediComm",
-            "<h1 style='color:red;'>" + message + "</h1>");
-        
-            response.end(htmlResponse);
-        }
-
-        var body =
-            "<form action='fileupload' method='post' enctype='multipart/form-data'>"+
-            "<input type='file' name='file'><br>"+
-            "<input type='submit'>"+
-            "</form>";
-        var htmlResponse = getHTMLStruct("MediComm", body);
-
-
-        var urlRequest = url.parse(request.url, true);
-        var name = "";
-
-
-
-        if(urlRequest.query.name != undefined) {
-            name = urlRequest.query.name;
-        }
-
-        response.write("Guten Tag " + name);
-
-        response.end(htmlResponse);
-    }
-}).listen(8080, "127.0.0.1");
-console.log("Webserver wird ausgeführt.");
+    console.log("Express app listening at http://%s:%s", host, port);
+});
 
 function getHTMLStruct(title, body){
     var htmlStruct = "<!DOCTYPE html>"
