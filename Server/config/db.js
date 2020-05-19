9c61e96934c81df7f032d3feb8ce198cc38a3e89
GeeHopper <stevengee@t-online.de>
2020-05-19 20:13:06 +0200

    added react for test purposes and added class to webserver to create a better code-structure

---
+++ Server/config/db.js
@@ -0,0 +1,20 @@
+//FILENAME : db.js
+
+const mongoose = require("mongoose");
+
+// Replace this with your MONGOURI.
+const MONGOURI = "mongodb+srv://MediComm:mcLx8se6d@medicomm-w3ph6.mongodb.net/test?retryWrites=true&w=majority";
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
--- Server/webserver.js
+++ Server/webserver.js
@@ -16,257 +16,330 @@ const cookieParser = require("cookie-parser");
 //self written requirements
 const date = require("./tools/dateTools");
 const dbTools = require("./tools/dbTools");
+const InitiateMongoServer = require("./config/db"); 
 
-//reading file from server
-/*fs.readFile("test.txt", function(error, data){
-    //console.log(data)//binary output
-    console.log(data.toString);
-})*/
+class Webserver
+{
 
-
-//writing file to server
-/*fs.writeFile("newFile.txt", "Inhalt der\nDatei", (error) => {
-    if(error)
-        throw error;
-    console.log("file was saved");
-});*/
-
-//passed console arguments
-/*var arguments = process.argv;
-console.log(arguments[2]);*/
-
-//colored console
-/*console.log("default");
-console.log(chalk.red("rot"));*/
-
-//dateoutput
-//console.log(date.germanDate());
-
-//proper logging function
-/*var s = util.format("Guten %s", "Tag");
-util.log(s);
-console.log(s)*/
+    //reading file from server
+    /*fs.readFile("test.txt", function(error, data){
+        //console.log(data)//binary output
+        console.log(data.toString);
+    })*/
 
 
-//initializing eventemitters
-var eventEmitter = new events.EventEmitter();
-var eventHandler = function() {
-    console.log("Event caught");
-}
-eventEmitter.on("myEvent", eventHandler);
-
-//connecting to db
-mongo.connect("mongodb://localhost:27017/", {useNewUrlParser: true},
-    function(error, client){
+    //writing file to server
+    /*fs.writeFile("newFile.txt", "Inhalt der\nDatei", (error) => {
         if(error)
             throw error;
-        else{
-            const collUser = client.db("medicomm").collection("users");
+        console.log("file was saved");
+    });*/
 
-            dbTools.createData(collUser,
-                [{vorname:"Alinaa", name:"münschhh"},
-                {vorname:"steven", name:"gee"}]);
+    //passed console arguments
+    /*var arguments = process.argv;
+    console.log(arguments[2]);*/
 
-            client.close();
-        }
-    }
-);
+    //colored console
+    /*console.log("default");
+    console.log(chalk.red("rot"));*/
 
+    //dateoutput
+    //console.log(date.germanDate());
 
-//express init
-var app = express();
+    //proper logging function
+    /*var s = util.format("Guten %s", "Tag");
+    util.log(s);
+    console.log(s)*/
 
-//extended: false->querystring,true->qs
-var urlParser = parser.urlencoded({extended:false})
-var upload = multer({dest: "uploads/"});
-
-app.use(cookieParser());
-
-/* cookie usage */
-
-app.get("/cookie_set", function(request, response){
-    response.cookie("hshl", "myValue", {expire: new Date() + 10000})
-        .end("set cookie");
-})
-
-app.get("/cookie_get", function(request, response){
-    response.cookie("hshl", "myValue", {expire: new Date() + 10000})
-        .end("set cookie");
-})
-
-app.get("/cookie_delete", function(request, response){
-    response.cookie("hshl", "myValue", {expire: new Date() + 10000})
-        .end("set cookie");
-})
-
-/* end cookie usage */
-
-app.get("/", function(request, response){
-    
-
-    //response.writeHead(200, {"content-type": "text/html; charset=utf-8"});
-    
-
-    //MySQL Database tools
-    /*
-    var con = mysql.createConnection({
-        host: "localhost",
-        database: "medicomm",
-        user: "root",
-        password: ""
-    });
-
-    var message = "msgtemplate";
-
-    eventEmitter.on("db-ready", sendResponse);
-    con.connect(function(error){
-        if(error){
-            message = "Connection error: " + error.message;
-            eventEmitter.emit("db-ready");
-            throw error;
-        }
-        else{
-            con.query("SELECT * FROM users",
-            function(error, result, fields){
-                if(error){
-                    
-                    message = "Query error: " + error.message;
-                    eventEmitter.emit("db-ready");
-                }
-                else{
-                    message = result[0].mail;
-                    eventEmitter.emit("db-ready");
-                }
-            });
-            
-        }
-    });
-
-    function sendResponse(){
-        var htmlResponse = getHTMLStruct("MediComm",
-        "<h1 style='color:red;'>" + message + "</h1>");
-    
-        response.end(htmlResponse);
-    }*/
-
-    //File upload
-    response.sendFile(path.join(__dirname+"/static/content/upload_test.html"));
-    
-    /*var body =
-        "<form action='fileupload' method='post' enctype='multipart/form-data'>"+
-        "<input type='file' name='file'><br>"+
-        "<input type='submit'>"+
-        "</form>";
-    var htmlResponse = getHTMLStruct("MediComm", body);
-
-
-    var urlRequest = url.parse(request.url, true);
-    var name = "";
-
-
-
-    if(urlRequest.query.name != undefined) {
-        name = urlRequest.query.name;
+    constructor()
+    {
+        this.init();
     }
 
-    response.send(htmlResponse);*/
+    initEventEmitters()
+    {
+        //initializing eventemitters
+        var eventEmitter = new events.EventEmitter();
+        var eventHandler = function() {
+            console.log("Event caught");
+        }
+        eventEmitter.on("myEvent", eventHandler);
+    }
 
-    //response.write("Guten Tag " + name);
 
-    //response.end(htmlResponse);
 
-});
-
-/* Express app reactions */
-
-//old version
-/*app.post("/fileupload", function(request, response){
-    var form = new formidable.IncomingForm();
-        form.parse(request, function(error, fields, files){
-            var oldpath = files.file.path;
-            var newpath = __dirname+"/files/"+files.file.name;
-
-            fs.rename(oldpath, newpath, function(error){
+    mongoConnect()
+    {
+    //connecting to db
+        mongo.connect("mongodb://localhost:27017/", {useNewUrlParser: true},
+            function(error, client){
                 if(error)
                     throw error;
-                response.write("File uploaded");
-                response.end();
-            });
+                else{
+                    const collUser = client.db("medicomm").collection("users");
+
+                    dbTools.createData(collUser,
+                        [{vorname:"Max", name:"Mustermann"},
+                        {vorname:"Tim", name:"Struppi"}]);
+
+                    client.close();
+                }
+            }
+        );
+    }
+
+    initAttributes()
+    {
+        //userdata for REST
+        this.userData = null;
+        //init express
+        this.app = express();
+        //extended: false->querystring,true->qs
+        this.urlParser = parser.urlencoded({extended:false});
+        this.upload = multer({dest: "uploads/"});
+    }
+
+    expressMethods()
+    {
+        /* cookie usage */
+        this.app.use(cookieParser());
+        this.app.get("/cookie_set", function(request, response){
+            response.cookie("hshl", "myValue", {expire: new Date() + 10000})
+                .end("set cookie");
         });
-});*/
 
-//app.post("file")
+        this.app.get("/cookie_get", function(request, response){
+            console.log(request.cookies);
+            response.end("Cookie: " + request.cookies["hshl"])
+        });
 
-app.post("/login", urlParser, function(request, response){
-    responseJSON = {
-        username:request.body.name,
-        password:request.body.password
-    };
-    response.end(JSON.stringify(responseJSON));
-});
+        this.app.get("/cookie_delete", function(request, response){
+            response.clearCookie("hshl");
+            response.end("Cookie deleted");
+        });
+        /* end cookie usage */
 
-//single file upload
-app.post("/fileupload", upload.single("file"), function(request, response){
-    var file = __dirname + "/" + request.file.originalname;
-    fs.rename(request.file.path,
-        request.file.destination+request.file.originalname,
-        (error) => {
-            if(error)
-                throw error;
+        /* restful function examples */
+        this.app.get("listUsers", function(request, responseJSON){
+            response.end(JSON.stringify(this.readUserData()));
+        });
+
+        this.app.get("/getUser=:id", function(request, response){
+            data = this.readUserData();
+            var user = data["user"+request.params.id];
+            response.end(JSON.stringify(user));
+        });
+
+        this.app.get("/addUser", function(request, response){
+            fs.readFile(__dirname+"/users.json", "utf8",
+                function(error, data){
+                    data = this.readUserData();
+                    data["user4"] = {
+                        "name" : "Mustermann",
+                        "vorname" : "Max",
+                        "email" : "max.mustermqnn@web.de"
+                    };
+                    response.end(JSON.stringify(data));
+                })
+        });
+
+        this.app.get("/removeUser=:id", function(request, response){
+            data = this.readUserData();
+            delete data["user"+request.params.id];
+
+            response.end(JSON.stringify(data));
+        });
+        /* end restful function examples */
+
+        this.app.get("/", function(request, response){
+            
+
+            //response.writeHead(200, {"content-type": "text/html; charset=utf-8"});
+            
+
+            //MySQL Database tools
+            /*
+            var con = mysql.createConnection({
+                host: "localhost",
+                database: "medicomm",
+                user: "root",
+                password: ""
+            });
+
+            var message = "msgtemplate";
+
+            eventEmitter.on("db-ready", sendResponse);
+            con.connect(function(error){
+                if(error){
+                    message = "Connection error: " + error.message;
+                    eventEmitter.emit("db-ready");
+                    throw error;
+                }
+                else{
+                    con.query("SELECT * FROM users",
+                    function(error, result, fields){
+                        if(error){
+                            
+                            message = "Query error: " + error.message;
+                            eventEmitter.emit("db-ready");
+                        }
+                        else{
+                            message = result[0].mail;
+                            eventEmitter.emit("db-ready");
+                        }
+                    });
+                    
+                }
+            });
+
+            function sendResponse(){
+                var htmlResponse = this.getHTMLStruct("MediComm",
+                "<h1 style='color:red;'>" + message + "</h1>");
+            
+                response.end(htmlResponse);
+            }*/
+
+            //File upload
+            response.sendFile(path.join(__dirname+"/static/content/upload_test.html"));
+            
+            /*var body =
+                "<form action='fileupload' method='post' enctype='multipart/form-data'>"+
+                "<input type='file' name='file'><br>"+
+                "<input type='submit'>"+
+                "</form>";
+            var htmlResponse = this.getHTMLStruct("MediComm", body);
+
+
+            var urlRequest = url.parse(request.url, true);
+            var name = "";
+
+
+
+            if(urlRequest.query.name != undefined) {
+                name = urlRequest.query.name;
+            }
+
+            response.send(htmlResponse);*/
+
+            //response.write("Guten Tag " + name);
+
+            //response.end(htmlResponse);
+
+        });
+
+        /* Express app reactions */
+
+        //old version
+        /*app.post("/fileupload", function(request, response){
+            var form = new formidable.IncomingForm();
+                form.parse(request, function(error, fields, files){
+                    var oldpath = files.file.path;
+                    var newpath = __dirname+"/files/"+files.file.name;
+
+                    fs.rename(oldpath, newpath, function(error){
+                        if(error)
+                            throw error;
+                        response.write("File uploaded");
+                        response.end();
+                    });
+                });
+        });*/
+
+        //app.post("file")
+
+        this.app.post("/login", this.urlParser, function(request, response){
+            //getting values of POST
             responseJSON = {
-                message: "File uploaded successfull",
-                filename: request.file.originalname
+                username:request.body.name,
+                password:request.body.password
+                //request.url.attribute for GET I think 
             };
             response.end(JSON.stringify(responseJSON));
         });
-});
 
-//multiple file upload
-app.post("/fileupload", upload.array("files"), function(request, response){
-    for (var index in request.files){
-        var regFile = request.files[index];
-        var file = __dirname + "/" + reqFile.originalname;
-        fs.rename(reqFile.path,
-            reqFile.destination+reqFile.originalname,
-            (error) => {
-                if(error)
-                    throw error;
-            });
+        //single file upload
+        this.app.post("/fileupload", this.upload.single("file"), function(request, response){
+            var file = __dirname + "/" + request.file.originalname;
+            fs.rename(request.file.path,
+                request.file.destination+request.file.originalname,
+                (error) => {
+                    if(error)
+                        throw error;
+                    responseJSON = {
+                        message: "File uploaded successfull",
+                        filename: request.file.originalname
+                    };
+                    response.end(JSON.stringify(responseJSON));
+                });
+        });
+
+        //multiple file upload
+        this.app.post("/fileupload", this.upload.array("files"), function(request, response){
+            for (var index in request.files){
+                var regFile = request.files[index];
+                var file = __dirname + "/" + reqFile.originalname;
+                fs.rename(reqFile.path,
+                    reqFile.destination+reqFile.originalname,
+                    (error) => {
+                        if(error)
+                            throw error;
+                    });
+            }
+            responseJSON = {
+                message: request.files.length+" files uploaded succesfully"
+            };
+            response.end(JSON.stringify(responseJSON));
+        });
+
+        /* end express app reactions */
     }
-    responseJSON = {
-        message: request.files.length+" files uploaded succesfully"
-    };
-    response.end(JSON.stringify(responseJSON));
-});
 
-/* end express app reactions */
+    startServer()
+    {
+        /* starting server using express */
+        var server = this.app.listen(8080, function() {
+            var host = server.address().address;
+            var port = server.address().port;
+
+            console.log("Express app listening at http://%s:%s", host, port);
+        });
+    }
+
+    init()
+    {
+        this.initEventEmitters();
+        this.mongoConnect();
+        this.initAttributes();
+        this.expressMethods();
+        this.startServer();
+    }
 
 
-/* starting server using express */
+    readUserData(renewData = false){
+        if(userData == null || renewData){
+            userData = JSON.parse(
+                fs.readFileSync(
+                    __dirname+"/users.json", "utf8")
+            );
+        }
+        return userData;
+    }
 
-var server = app.listen(8080, function() {
-    var host = server.address().address;
-    var port = server.address().port;
+    getHTMLStruct(title, body){
+        var htmlStruct = "<!DOCTYPE html>"
+        + "<html>"
+            + "<head>"
+                + "<meta charset='utf-8'>"
+                + "<title>" + title + "</title>"
+            + "</head>"
+            + "<body>"
+                + body
+            + "</body>"
+        + "</html>";
 
-    console.log("Express app listening at http://%s:%s", host, port);
-});
+        return htmlStruct;
+    }
 
-
-
-
-
-function getHTMLStruct(title, body){
-    var htmlStruct = "<!DOCTYPE html>"
-    + "<html>"
-        + "<head>"
-            + "<meta charset='utf-8'>"
-            + "<title>" + title + "</title>"
-        + "</head>"
-        + "<body>"
-            + body
-        + "</body>"
-    + "</html>";
-
-    return htmlStruct;
 }
 
+var webserver = new Webserver();
